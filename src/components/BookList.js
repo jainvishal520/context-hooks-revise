54260f02a4751be3671fd22a6abf9f4c8b1bef1b
“Vishal <jainvishal520@gmail.com>
2020-07-05 22:46:23 +0530

    Accessing Context - Context consumer

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,24 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
-
-class BookList extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { isLightTheme, light, dark } = this.context;
-    const theme = isLightTheme ? light : dark;
-    return (
-      <div
-        className="book-list"
-        style={{ color: theme.syntax, background: theme.bg }}
-      >
-        <ul>
-          <li style={{ background: theme.ui }}>the way of kings</li>
-          <li style={{ background: theme.ui }}>the name of the wind</li>
-          <li style={{ background: theme.ui }}>the final empire</li>
-        </ul>
-      </div>
-    );
-  }
-}
+// this consumer expects us to pass in a function and has parameter context
+const BookList = () => {
+  return (
+    <ThemeContext.Consumer>
+      {(context) => {
+        const { isLightTheme, light, dark } = context;
+        const theme = isLightTheme ? light : dark;
+        return (
+          <div
+            className="book-list"
+            style={{ color: theme.syntax, background: theme.bg }}
+          >
+            <ul>
+              <li style={{ background: theme.ui }}>the way of kings</li>
+              <li style={{ background: theme.ui }}>the name of the wind</li>
+              <li style={{ background: theme.ui }}>the final empire</li>
+            </ul>
+          </div>
+        );
+      }}
+    </ThemeContext.Consumer>
+  );
+};
 
 export default BookList;
