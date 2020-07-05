2ae0ca054028994461bdd0c9e591ff22a84e2462
“Vishal <jainvishal520@gmail.com>
2020-07-05 23:49:29 +0530

    hooks app initial commit

---
+++ hooksapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+import SongsList from "./components/SongsList";
+
+function App() {
+  return (
+    <div className="App">
+      <SongsList />
+    </div>
+  );
+}
+
+export default App;
