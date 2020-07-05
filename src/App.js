d29f3277a013c9a837c7062d9405c70d8adb3e88
“Vishal <jainvishal520@gmail.com>
2020-07-05 23:21:12 +0530

    Creating Multiple Contexts

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Navbar from "./components/Navbar";
 import BookList from "./components/BookList";
 import ThemeContextProvider from "./contexts/ThemeContext";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./contexts/AuthContext";
 function App() {
   return (
     <div className="App">
       <ThemeContextProvider>
         {/* Both navbar and booklist are attached to props of ThemeContextProvider as wrapped inside */}
-        <Navbar />
-        <BookList />
-        <ThemeToggle />
+        <AuthContextProvider>
+          <Navbar />
+          <BookList />
+          <ThemeToggle />
+        </AuthContextProvider>
       </ThemeContextProvider>
     </div>
   );
