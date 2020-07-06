867456a510ab08c96949ebb49c46a92e857f02c3
“Vishal <jainvishal520@gmail.com>
2020-07-06 19:06:12 +0530

    Converting context to Functional Components

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,24 +1,46 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, Component, useState } from "react";
 export const AuthContext = createContext();
 
-class AuthContextProvider extends Component {
-  state = {
+// // -------------------- Class based context --------------------
+
+// class AuthContextProvider extends Component {
+//   state = {
+//     isAuthenticated: false,
+//   };
+//   toggleAuth = () => {
+//     this.setState({
+//       isAuthenticated: !this.state.isAuthenticated,
+//     });
+//   };
+//   render() {
+//     return (
+//       <AuthContext.Provider
+//         value={{ ...this.state, toggleAuth: this.toggleAuth }}
+//       >
+//         {this.props.children}
+//       </AuthContext.Provider>
+//     );
+//   }
+// }
+// // -------------------- END Class based context --------------------
+
+// -------------------- Functional component based context --------------------
+const AuthContextProvider = (props) => {
+  const [auth, setAuth] = useState({
     isAuthenticated: false,
-  };
-  toggleAuth = () => {
-    this.setState({
-      isAuthenticated: !this.state.isAuthenticated,
+  });
+  const toggleAuth = () => {
+    setAuth({
+      isAuthenticated: !auth.isAuthenticated,
     });
   };
-  render() {
-    return (
-      <AuthContext.Provider
-        value={{ ...this.state, toggleAuth: this.toggleAuth }}
-      >
-        {this.props.children}
-      </AuthContext.Provider>
-    );
-  }
-}
+  return (
+    <AuthContext.Provider value={{ ...auth, toggleAuth }}>
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+// -------------------- END Functional component based context --------------------
 
 export default AuthContextProvider;
--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,34 +1,57 @@
-import React, { createContext, Component } from "react";
+import React, { createContext, Component, useState } from "react";
 
 export const ThemeContext = createContext();
 
-// state in this component will be the data which we want to share b/w components
+// // -------------------- Class based context --------------------
+// // state in this component will be the data which we want to share b/w components
 
-class ThemeContextProvider extends Component {
-  state = {
+// class ThemeContextProvider extends Component {
+//   state = {
+//     isLightTheme: true,
+//     light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
+//     dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+//   };
+//   toggleTheme = () => {
+//     this.setState({
+//       isLightTheme: !this.state.isLightTheme,
+//     });
+//   };
+//   render() {
+//     return (
+//       // this value property will contain data which we want to share to components this wraps - value will be an object
+//       <ThemeContext.Provider
+//         value={{ ...this.state, toggleTheme: this.toggleTheme }}
+//       >
+//         {/* // Inside this provider we want to have those components will we want to provide this context  */}
+//         {this.props.children}
+//         {/* props of ThemeContextProvider */}
+//       </ThemeContext.Provider>
+//     );
+//   }
+// }
+
+// As the state changes by update func. ... the value being passed changes ... and therefore the components which consume this data updates
+// --------------------END Class component based context --------------------
+
+// -------------------- Functional component based context --------------------
+const ThemeContextProvider = (props) => {
+  const [theme, setTheme] = useState({
     isLightTheme: true,
     light: { syntax: "#555", ui: "#ddd", bg: "#eee" },
     dark: { syntax: "#ddd", ui: "#333", bg: "#555" },
+  });
+  const toggleTheme = () => {
+    setTheme({ ...theme, isLightTheme: !theme.isLightTheme });
   };
-  toggleTheme = () => {
-    this.setState({
-      isLightTheme: !this.state.isLightTheme,
-    });
-  };
-  render() {
-    return (
-      // this value property will contain data which we want to share to components this wraps - value will be an object
-      <ThemeContext.Provider
-        value={{ ...this.state, toggleTheme: this.toggleTheme }}
-      >
-        {/* // Inside this provider we want to have those components will we want to provide this context  */}
-        {this.props.children}
-        {/* props of ThemeContextProvider */}
-      </ThemeContext.Provider>
-    );
-  }
-}
+  return (
+    // this value property will contain data which we want to share to components this wraps - value will be an object
+    <ThemeContext.Provider value={{ ...theme, toggleTheme }}>
+      {/* // Inside this provider we want to have those components will we want to provide this context  */}
+      {props.children}
+      {/* props of ThemeContextProvider */}
+    </ThemeContext.Provider>
+  );
+};
+// -------------------- END Functional component based context --------------------
 
 export default ThemeContextProvider;
-
-// As the state changes by update func. ... the value being passed changes ... and therefore the components which consume this data updates
