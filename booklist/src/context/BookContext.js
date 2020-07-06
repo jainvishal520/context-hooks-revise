0bf02d49fd294196d6d21b65dbeb4aa1a3670935
“Vishal <jainvishal520@gmail.com>
2020-07-07 00:00:23 +0530

    Adding local storage

--- booklist/src/context/BookContext.js
+++ booklist/src/context/BookContext.js
@@ -1,12 +1,18 @@
-import React, { createContext, useState, useReducer } from "react";
+import React, { createContext, useState, useReducer, useEffect } from "react";
 import { BookReducer } from "../reducers/BookReducer";
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
   //   const [books, setBooks] = useState([]);
-  const [books, dispatch] = useReducer(BookReducer, []);
-
+  // UseReducer takese in 3rd arg as an cb func which return a default value if 2nd arg is empty
+  const [books, dispatch] = useReducer(BookReducer, [], () => {
+    const localData = localStorage.getItem("books");
+    return localData ? JSON.parse(localData) : [];
+  });
+  useEffect(() => {
+    localStorage.setItem("books", JSON.stringify(books));
+  }, [books]);
   //   const addBook = (book) => {
   //     setBooks([...books, book]);
   //   };
